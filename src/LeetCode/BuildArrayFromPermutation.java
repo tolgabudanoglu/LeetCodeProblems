e4de8697229adf9d6e978b9ef45c24ef814a79ca
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-04-04 16:29:36 +0300

    BuildArrayFromPermutation 1920

---
+++ src/LeetCode/BuildArrayFromPermutation.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class BuildArrayFromPermutation {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int[] buildArray(int[] nums) {
+        aPermutation(nums,0);
+
+        return nums;
+    }
+
+    static void aPermutation(int[] nums, int start){
+        if(start<nums.length){
+            int temp=nums[start];
+            int result=nums[temp];
+            aPermutation(nums,start+1);
+            nums[start]=result;
+        }
+
+    }
+
+
+}
---
+++ src/LeetCode/aa.java
@@ -0,0 +1,7 @@
+package LeetCode;
+
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("H");
+    }
+}
