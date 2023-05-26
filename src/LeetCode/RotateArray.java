9bd2f7adeb419aae634a55e3f9ecdd8dc1fc499f
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-05-26 18:47:42 +0300

    189. Rotate Array

---
+++ src/LeetCode/RotateArray.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void rotate(int[] nums, int k) {
+
+        if (k > nums.length){
+            k=k%nums.length;
+        }
+
+        int[] result = new int[nums.length];
+
+        for (int i=0;i<k;i++){
+            result[i] = nums[nums.length -k + i];
+        }
+
+        int j = 0;
+        for (int i = k; i < nums.length; i++) {
+            result[i] = nums[j];
+            j++;
+        }
+
+        System.arraycopy(result,0,nums,0,nums.length);
+    }
+
+
+}
+
+
---
+++ src/LeetCode/setMismatch.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class setMismatch {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] findErrorNums(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[2];
+
+
+        return result;
+    }
+}
+
