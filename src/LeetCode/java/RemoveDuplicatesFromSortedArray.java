a5d7d0a8b3c9e9a85a5a9d8803c2ba4b94f35d7c
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-05-29 22:26:14 +0300

    26. Remove Duplicates from Sorted Array

---
+++ src/LeetCode/java/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+package LeetCode.java;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int k = 1; // benzersiz elemanların sayısı
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
