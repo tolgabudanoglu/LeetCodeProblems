58e94c64331bb70591dfa9e6a090d2c4987a6e4b
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-06-02 18:22:47 +0300

    344. Reverse String

---
+++ src/LeetCode/java/ReverseString.java
@@ -0,0 +1,22 @@
+package LeetCode.java;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+    }
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+
+            left++;
+            right--;
+        }
+    }
+}
