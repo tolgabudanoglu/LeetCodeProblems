9a81b44ed24ce3f30a202e40ec7eddfe3dc64163
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-03-22 19:12:29 +0300

    2469. Convert the Temperature

---
+++ src/LeetCode/kotlin/ConvertTheTemperature.kt
@@ -0,0 +1,13 @@
+package LeetCode.kotlin
+
+fun main() {
+
+}
+
+fun convertTemperature(celsius: Double): DoubleArray {
+
+    val kelvin = celsius + 273.15
+    val fahrenheit = celsius * 1.80 + 32.00
+
+    return doubleArrayOf(kelvin,fahrenheit)
+}
